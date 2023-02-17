1dab88b2e99be3984887c86db12d77c3b34e3391
Derek Zhou <derekzhou1105@gmail.com>
2023-02-16 19:25:05 -0500

    string start

---
+++ 6 vector容器/6 vector容器/P189_string容器-构造函数.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+void test18901()
+{
+	string s1; // Ä¬ÈÏ¹¹Ôì
+
+	const char* str = "hello world";
+	string s2(str);
+
+	cout << "s2 = " << s2 << endl;
+
+	string s3(s2);
+	cout << "s3 = " << s3 << endl;
+
+	string s4(10, 'a');
+	cout << "s4 = " << s4 << endl;
+}
+
+int main(void)
+{
+	test18901();
+
+	return 0;
+}
